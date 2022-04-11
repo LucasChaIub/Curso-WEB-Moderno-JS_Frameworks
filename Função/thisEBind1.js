069449c5bd752eecf89a609eff056fbe9798a1f3
Lucas Chalub <lucas.alves.chalub@gmail.com>
2022-04-11 15:49:27 -0300

    Capítulo de Funções

---
+++ Função/thisEBind1.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    saudacao: "Bom dia",
+    falar() {
+        console.log(this.saudacao);      
+    }
+};
+
+pessoa.falar();
+const falar = pessoa.falar;
+falar(); // Conflito entre Paradigmas: Funcional e POO
+
+const falarDePessoa = pessoa.falar.bind(pessoa);
+falarDePessoa();
